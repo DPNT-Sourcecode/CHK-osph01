5a33e1c217d455c72ce3b23bfebf13d55ba8bc5d
sbx_user1051 <sbx_user1051@169.254.97.165>
2024-11-03 19:47:37 +0000

    Sun Nov 03 19:47:37 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,4 +1,18 @@
 
+PRICES = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15
+}
+
+OFFERS = {
+    "A": (3, 130),
+    "B": (2, 45)
+}
+
+def convert_skus_to_useful(skus):
+    {}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
@@ -8,5 +22,12 @@ def checkout(skus):
     ##       if any unknown items then return -1
     ##       (suspected stock check in next few rounds)
     ##       have knowledge of prices
-    ##       use map of sales to work out prices
+    ##       use map of sales to work out total price (divmod)
+
+    sku_map = convert_skus_to_useful(skus)
+    price = 0
+    
+    for item in s
+
+    return price
 
