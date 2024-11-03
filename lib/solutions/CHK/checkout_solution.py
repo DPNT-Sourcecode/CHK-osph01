e502513dbddad65238b3a81c6bd60e9ae91212e3
sbx_user1051 <sbx_user1051@169.254.97.165>
2024-11-03 19:44:37 +0000

    Sun Nov 03 19:44:37 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,10 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    ## TODO: find out format of skus input
+    ##       turn into map of items needed
+    ##       if any unknown items then return -1
+    ##       (suspected stock check in next few rounds)
+    ##       have knowledge of prices
+    ##       use map of sales to work out prices
+
