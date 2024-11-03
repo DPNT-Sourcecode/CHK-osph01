4e7b81a756d4f57223c048ee5be95984a7e74adf
sbx_user1051 <sbx_user1051@169.254.97.165>
2024-11-03 20:08:11 +0000

    Sun Nov 03 20:08:11 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -12,6 +12,14 @@ BUNDLE_OFFERS = {
     "B": [(2, 45)]
 }
 
+def sku_ordering(sku_map):
+    keys = sku_map.keys
+    if "E" in keys:
+        keys.remove("E")
+        keys.insert(0, "E")
+    
+    return keys
+
 def convert_skus_to_useful(skus):
     out = {}
 
@@ -26,20 +34,22 @@ def checkout(skus):
     sku_map = convert_skus_to_useful(skus)
     price = 0
     
-    for item in sku_map:
+    for item in sku_ordering(sku_map):
         num_item = sku_map[item]
         item_price = PRICES.get(item)
         if not item_price:
             return -1
 
-        offer = OFFERS.get(item)
+        offers = BUNDLE_OFFERS.get(item)
 
-        if offer:
-            bundle_num, new_price = offer
-            num_offer_claimed, num_item = divmod(num_item, bundle_num)
-            price += num_offer_claimed * new_price
+        if offers:
+            for offer in offers:
+                bundle_num, new_price = offer
+                num_offer_claimed, num_item = divmod(num_item, bundle_num)
+                price += num_offer_claimed * new_price
 
         price += num_item * item_price
 
     return price
+
 
