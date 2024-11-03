bc59d2e743d4c1c7a3467d5badbf6200819d36dc
sbx_user1051 <sbx_user1051@169.254.97.165>
2024-11-03 19:50:37 +0000

    Sun Nov 03 19:50:37 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -27,7 +27,19 @@ def checkout(skus):
     sku_map = convert_skus_to_useful(skus)
     price = 0
     
-    for item in s
+    for item in sku_map:
+        num_item = sku_map[item]
+        price = PRICES.get(item)
+
+        if not price:
+            return -1
+
+        offer = OFFERS.get(item)
+
+        if offer:
+            bundle_num, new_price = offer
+            num_offer_claimed, num_item = divmod(num_item, bundle_num)
+            
 
     return price
 
